70c07ac46534cf7a13b8e5de36de811cf26fa9d3
Max Zelinka <fourthwalljoke@gmail.com>
2017-10-25 22:30:18 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {
+  Beatles:"Come Together",
+  Paramore:"Fake Happy",
+  Panic at the disco: "LA Devotee"
+}
+
+function updatePlaylist(playlist, artist, song) {
+  return playlist.artist = song
+}
