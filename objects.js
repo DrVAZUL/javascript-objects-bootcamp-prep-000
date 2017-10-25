57b1317f1015b557df4358061a45d19eccd494f4
Max Zelinka <fourthwalljoke@gmail.com>
2017-10-25 22:33:13 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,8 +1,4 @@
-var playlist = {
-  Beatles: "Come Together",
-  Paramore: "Fake Happy",
-  Panic at the disco: "LA Devotee"
-}
+var playlist = {Beatles: "Come Together", Paramore: "Fake Happy", Panic at the disco: "LA Devotee"}
 
 function updatePlaylist(object, artist, song) {
   return object.artist = song
